542b225110d3b822fddf2302360e135a7f6bad67
Ivan Iurii <iuriiiv302@gmail.com>
2019-05-29 10:48:03 +0300

    update models

--- foundation/models.py
+++ foundation/models.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 
 from django.db import models
+import factory.django
 
 
 class Statement(models.Model):
     name = models.CharField(max_length=15)
-    first_name = models.CharField(max_length=15)
-    number = models.IntegerField()
+    surname = models.CharField(max_length=15)
+    phone_number = models.CharField(max_length=32)
     date_create_statement = models.DateTimeField(null=True, blank=True, default=datetime.now())
     INPROCESS = 'inprocess'
     FINISHED = 'finished'
@@ -15,3 +16,13 @@ class Statement(models.Model):
         (FINISHED, 'finished'),
     ]
     status_pass = models.CharField(max_length=10, choices=STATUS_CHOICES)
+
+
+class StatementFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Statement
+
+    name = factory.Faker('name')
+    surname = factory.Faker('surname')
+    phone_number = factory.Faker('phone_number')
+    status_pass = factory.Faker("What process is your passport in?")
