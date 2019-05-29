9da00258af0c8d9c5b6ce09cfe967772c2675a82
Ivan Iurii <iuriiiv302@gmail.com>
2019-05-29 10:17:01 +0300

    Add models

--- foundation/models.py
+++ foundation/models.py
@@ -1,3 +1,17 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Statement(models.Model):
+    name = models.CharField(max_length=15)
+    first_name = models.CharField(max_length=15)
+    number = models.IntegerField()
+    date_create_statement = models.DateTimeField(null=True, blank=True, default=datetime.now())
+    INPROCESS = 'inprocess'
+    FINISHED = 'finished'
+    STATUS_CHOICES = [
+        (INPROCESS, 'inprocess'),
+        (FINISHED, 'finished'),
+    ]
+    status_pass = models.CharField(max_length=10, choices=STATUS_CHOICES)
